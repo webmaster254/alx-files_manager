c5833c7a86e5f92766f98b8601e57daa3d773d62
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2023-06-30 21:21:29 +0300

    redis

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+
+    this.client.on('error', (error) => {
+      console.error('Redis client not connected to the server:', error.message);
+      this.isClientConnected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
