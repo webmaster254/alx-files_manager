f41800b6eaa0da5c13610fae91e48b545bd05e8f
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2023-07-11 17:10:11 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,22 @@
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = Router();
+const router = express.Router();
 
+// the get Routes
 router.get('/status', AppController.getStatus);
-
 router.get('/stats', AppController.getStats);
-
-router.post('/users', UsersController.postNew);
-
 router.get('/connect', AuthController.getConnect);
-
 router.get('/disconnect', AuthController.getDisconnect);
-
 router.get('/users/me', UsersController.getMe);
-
-router.post('/files', FilesController.postUpload);
-
 router.get('/files/:id', FilesController.getShow);
-
 router.get('/files', FilesController.getIndex);
 
-router.put('/files/:id/publish', FilesController.putPublish);
-
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-router.get('/files/:id/data', FilesController.getFile);
+// the post Routes
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
